3ec429829c4b6d8fe63673a1e7d8c3e767ee24a0
shivam saini <shivamsainier98@gmail.com>
2021-09-13 11:01:46 +0530

    Form Hendling

--- sainiapp/src/Components/Forms.js
+++ sainiapp/src/Components/Forms.js
@@ -1,26 +1,43 @@
 import React, { Component } from 'react'
-
+import style from "./appStyle.module.css"
 export class Forms extends Component {
     constructor(props) {
         super(props)
         this.state = {
-             username:''
+             username:'',
+             comments:''
         }
     }
     setUsername=(event)=>{
-        this.setState(()=>{
-            this.setState({
-                username:event.target.value      
-            })
+        this.setState({
+            username:event.target.value
         })
+    }
+    setComments=(event)=>{
+        this.setState({
+            comments:event.target.value
+        })
+    }
+    formSubmit=(event)=>{
+        alert(this.state.username?`username ${this.state.username}`:"")
     }
 
     render() {
         return (
             <div>
-                <label>Username</label>
-                <input type="text" placeholder="Enter Username" value={this.state.username} onChange={this.setUsername}></input>
-                <h1>{this.state.username}</h1>
+                <form onSubmit={this.formSubmit}>
+                    <div>
+                    <label>Username </label>
+                        <input type="text" value={this.state.username} placeholder="Enter Username" onChange={this.setUsername}></input>
+                    </div>
+                    <div>
+                    <label> Comments </label>
+                        <textarea placeholder="Enter Comments" onChange={this.setComments} value={this.state.comments}></textarea>
+               </div>
+               <input type="submit" value="Submit"></input>
+                </form>
+                <h3 className={style.error}>{this.state.username}</h3>
+                <h4 className={style.success}>{this.state.comments}</h4>
             </div>
         )
     }
