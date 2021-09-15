60fec97f0a3a72098f38cd94853d2391b5b98f07
shivam saini <shivamsainier98@gmail.com>
2021-09-15 13:37:44 +0530

    Ref Element

--- sainiapp/src/Components/RefDemo.js
+++ sainiapp/src/Components/RefDemo.js
@@ -4,18 +4,35 @@ class RefDemo extends Component {
     constructor(props) {
         super(props)
         this.inputRef=React.createRef()
+        this.cref=null
+        this.select=select=>{
+            this.cref=select
+        }
+
+       this.state={
+           name:''
+       }
     }
     componentDidMount(){
-        console.log(this.inputRef)
-        this.inputRef.current.value="shel"
+        if(this.cref){
+            this.cref.value=this.state.name
+        }
     }
+    change=(event)=>{
+        this.setState({
+            name:event.target.value
+        })
+    }
+
     render() {
 
         return (
             <div>
              <label>enter Something</label> <br>
              </br>  
-             <input type="text" ref={this.inputRef}></input>
+             <input type="text" ref={this.inputRef} onChange={this.change}></input><br />
+             <input type="text" ref={this.select}></input>
+             <h1>{this.state.name}</h1>
             </div>
         )
     }
