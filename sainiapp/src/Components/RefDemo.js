dda0c0a9cb7607761e00457d6070a539c806bc28
shivam saini <shivamsainier98@gmail.com>
2021-09-16 10:10:59 +0530

    Forward refs

--- sainiapp/src/Components/RefDemo.js
+++ sainiapp/src/Components/RefDemo.js
@@ -1,30 +1,12 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class RefDemo extends Component {
-    constructor(props) {
-        super(props)
-        this.inputref=React.createRef()
-        }
+const RefDemo=React.forwardRef((props,ref)=>{
+    return(
+        <div>
+            <input type="text" ref={ref}></input>
+        </div>
+    )
 
-    focus(){
-        this.inputref.current.focus()
-    }
-
-    helloWorld(){
-        this.inputref.current.value="Hello World"
-    }
-
-    render() {
-        return (
-            <div>
-             <label>enter Something</label> <br>
-             </br>  
-             <input type="text" ref={this.inputref}></input><br />
-             
-             <button onClick={()=>{this.helloWorld()}}>Hello World</button>
-                         </div>
-        )
-    }
-}
+})
 
 export default RefDemo
--- sainiapp/src/Components/Refs.js
+++ sainiapp/src/Components/Refs.js
@@ -2,10 +2,22 @@ import React, { Component } from 'react'
 import RefDemo from './RefDemo'
 
 class Refs extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             
+        }
+        this.input=React.createRef()
+    }
+    focus(){
+        this.input.current.value="Hello world"
+    }
+    
     render() {
         return (
             <div>
-                <RefDemo />
+                <RefDemo ref={this.input}/>
                 <button onClick={()=>this.focus()}>Click</button>
             </div>
         )
