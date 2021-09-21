7af2a54ae8397c3b487987bd2690aae62e510fe6
shivam saini <shivamsainier98@gmail.com>
2021-09-21 11:13:05 +0530

    use UseState Hook

--- sainiapp/src/Expense/E.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function E() {
-    return React.createElement(
-        'div',{},React.createElement('h1',{},"hello World!")
-    )
-}
-
-export default E
--- sainiapp/src/Expense/Expenses.js
+++ sainiapp/src/Expense/Expenses.js
@@ -1,17 +1,25 @@
-import React from 'react'
+import React,{useState} from 'react'
 import "./Style.css"
 
-function Expenses(props) {
 
+
+
+function Expenses(props) {
+    const [value,setValue]=useState(0)
+    console.log("Component Render ")
+    let title=props.title
+    const click=()=>{
+       setValue(value+1)
+    }
     return (
         <div className="expense-item">
             <div>{props.date}</div>
             <div>year</div>
             <div>Day</div><div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">{props.price}</div>
             </div>
-            
+            <button onClick={click}>{value}</button>
         </div>
     )
 }
