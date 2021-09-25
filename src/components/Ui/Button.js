07e66bcc7e19a46d5b59c99d3ab69557fdedb44f
shivam saini <shivamsainier98@gmail.com>
2021-09-25 21:47:49 +0530

    initial Commit

---
+++ src/components/Ui/Button.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import classes from "../Ui/Button.module.css"
+
+function Button(props) {
+
+    return (
+        <div>
+            <button className={classes.button}
+             type={props.type || 'button'} onClick={props.onClick}>{props.children}</button>
+        </div>
+    )
+}
+
+export default Button
