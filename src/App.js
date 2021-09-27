c65e7dc988ec2e8aad19fc52d36d034526b535e2
shivam saini <59383963+shivam007-ss@users.noreply.github.com>
2021-09-27 10:06:00 +0530

    Add UserList Component to show User's List

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import AddUser from './components/User/AddUser';
+import UserList from './components/User/UserList';
 
 function App() {
   return (
     <div>
      <AddUser />
+     <UserList />
     </div>
   );
 }
