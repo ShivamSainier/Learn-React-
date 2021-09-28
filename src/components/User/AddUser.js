66f3e250ee003c3b6f8b9c77de088f139745c5e6
shivam saini <shivamsainier98@gmail.com>
2021-09-28 12:06:03 +0530

    Final Submittion

--- src/components/User/AddUser.js
+++ src/components/User/AddUser.js
@@ -1,45 +1,46 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import Button from '../Ui/Button';
 import Card from '../Ui/Card';
 import ErrorModel from '../Ui/ErrorModel';
 import classes from "./AddUSer.module.css"
 
 function AddUser(props) {
-    const [user,setUser]=useState("")
-    const [userage,setAge]=useState("")
-    const [error,setError]=useState()
+    const [user, setUser] = useState("")
+    const [userage, setAge] = useState("")
+    const [error, setError] = useState()
 
 
-    const AddUserHendler=(event)=>{
+    const AddUserHendler = (event) => {
         event.preventDefault();
-        
-        if(user.length===0 || userage===0){
+
+        if (user.length === 0 || userage === 0) {
             setError({
-                title:'Invalid Input',
-                message:'Please Enter Valid name and Age'
+                title: 'Invalid Input',
+                message: 'Please Enter Valid name and Age'
             })
             return;
         }
-       if(userage.length<10 ){
-           setError({
-               title:'Invalid Input',
-               message:'Please Enter Valid Age (>0)'
+        if (userage.length < 10) {
+            setError({
+                title: 'Invalid Input',
+                message: 'Please Enter Valid Age (>0)'
 
-           })
-           return;
-       }
-        props.onAddUser(user,userage);
-        console.log(user,userage)
+            })
+            return;
+        }
+        props.onAddUser(user, userage);
+        console.log(user, userage)
         setUser("")
-        setAge("")}
-    const usernamehendler=(event)=>{
+        setAge("")
+    }
+    const usernamehendler = (event) => {
         setUser(event.target.value)
     }
-    const agehendler=(event)=>{
+    const agehendler = (event) => {
         setAge(event.target.value)
     }
 
-    const errorHendler=()=>{
+    const errorHendler = () => {
         setError(null);
     }
 
@@ -47,17 +48,17 @@ function AddUser(props) {
         <div>
             {error && <ErrorModel title={error.title} message={error.message} onEvent={errorHendler} />}
             <Card className={classes.input}>
-            <form onSubmit={AddUserHendler}>
-                <label htmlFor="">User name</label>
-                <input id="username" type="text" onChange={usernamehendler} value={user} />
-                <label htmlFor="Age">Age (Years)</label>
-                <input type="number" id="age"  onChange={agehendler} value={userage}/>
-                <Button type="submit"> Add USer</Button>
-                
-            </form>
+                <form onSubmit={AddUserHendler}>
+                    <label htmlFor="">User name</label>
+                    <input id="username" type="text" onChange={usernamehendler} value={user} />
+                    <label htmlFor="Age">Age (Years)</label>
+                    <input type="number" id="age" onChange={agehendler} value={userage} />
+                    <Button type="submit"> Add USer</Button>
+
+                </form>
             </Card>
-            
-        </div>  
+
+        </div>
     )
 }
 
